269ca9f850c4d7bd6c47f852bd33d99aadf31191
e1berd <72397796+Elberd-Chakhiev@users.noreply.github.com>
2022-09-19 00:09:58 +0300

    add animation to header menu

--- src/components/Header/Menu/HeaderItem.vue
+++ src/components/Header/Menu/HeaderItem.vue
@@ -1,6 +1,23 @@
 <template>
   <li>
-    <a :href="href">
+    <a
+      class="
+        relative
+        before:content-['']
+        before:absolute
+        before:-bottom-2
+        before:left-0
+        before:border-b-2
+        before:w-0
+        before:h-1
+        before:transition-all
+        hover:before:w-full
+        before:border-[#1860CF]
+        hover:text-[#1860CF]
+        transiton-colors
+      "
+      :href="href"
+    >
       {{ text }}
     </a>
   </li>
