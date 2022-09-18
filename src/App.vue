524a770d29041a5b3bcd52b962d744c9af5fd454
Lida <garbakovalida@mail.ru>
2022-09-18 22:41:32 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,17 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+ <PrewieVideo dog="cat">
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+ </PrewieVideo>
+ <PrewieVideo dog="111"></PrewieVideo>
+ <PrewieVideo></PrewieVideo>
+ <PrewieVideo></PrewieVideo>
+ <PrewieVideo></PrewieVideo>
+</template>
+<script>
+import PrewieVideo from "./components/PrewieVideo.vue"
+export default {
+  components: {
+    PrewieVideo
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
